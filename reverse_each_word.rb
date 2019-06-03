b429fc3367f0a98aa8f2f0df96b3af1ac813f565
tommaso auerbach <tsa302@nyu.edu>
2019-06-03 01:10:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
 
-def reverse_each_word(cool)
-  
+def reverse_each_word(string)
+  original_array = string.split(" ")
+  return array []
+  original_array.each do |string|
+    return_array << string.reverse
+  end
+  return_array.join("")
+end
